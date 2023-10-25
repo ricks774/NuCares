df957bc8e54de96f90a9b52ebeb0ef2e5a07da41
wendy03 <wendysu03@gmail.com>
2023-10-25 14:10:21 +0800

    feat: Nutritionist Add Model

--- NuCares/Models/Nutritionist.cs
+++ NuCares/Models/Nutritionist.cs
@@ -80,9 +80,29 @@ namespace NuCares.Models
         [Display(Name = "課程介紹")]
         public string CourseIntro { get; set; }
 
-        [JsonIgnore]
-        [Display(Name = "營養師帳戶")]
-        public virtual ICollection<Bank> Banks { get; set; }
+        [MaxLength(100)]
+        [Display(Name = "其他通訊軟體1")]
+        public string Option1{ get; set; }
+
+        [MaxLength(100)]
+        [Display(Name = "其他通訊軟體 ID 1")]
+        public string OptionId1 { get; set; }
+
+        [MaxLength(100)]
+        [Display(Name = "其他通訊軟體21")]
+        public string Option2 { get; set; }
+
+        [MaxLength(100)]
+        [Display(Name = "其他通訊軟體 ID 2")]
+        public string OptionId2 { get; set; }
+
+        [MaxLength(100)]
+        [Display(Name = "其他通訊軟體31")]
+        public string Option3 { get; set; }
+
+        [MaxLength(100)]
+        [Display(Name = "其他通訊軟體 ID 3")]
+        public string OptionId3 { get; set; }
 
         [JsonIgnore]
         [Display(Name = "營養師課程方案")]
