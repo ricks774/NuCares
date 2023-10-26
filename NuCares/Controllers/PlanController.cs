170c894a6772d81151dcfe5b34c4c25e5558a9dc
wendy03 <wendysu03@gmail.com>
2023-10-26 13:46:26 +0800

    feat: Add nu create plan API

---
+++ NuCares/Controllers/PlanController.cs
@@ -0,0 +1,85 @@
+﻿using NuCares.Models;
+using NuCares.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace NuCares.Controllers
+{
+    public class PlanController : ApiController
+    {
+        private readonly NuCaresDBContext db = new NuCaresDBContext();
+
+        #region  "新增API"
+        /// <summary>
+        /// 新增營養師課程方案
+        /// </summary>
+        /// <param name="planView">新增方案</param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("nu/plan")]
+        [JwtAuthFilter]
+        public IHttpActionResult CreatePlan([FromBody] PlanView planView)
+        {
+            var userToken = JwtAuthFilter.GetToken(Request.Headers.Authorization.Parameter);
+            int id = (int)userToken["Id"];
+            bool isNutritionist = (bool)userToken["IsNutritionist"];
+            bool checkUser = db.Nutritionists.Any(n => n.UserId == id);
+            if (!isNutritionist || !checkUser)
+            {
+                return Content(HttpStatusCode.BadRequest, new
+                {
+                    StatusCode = 403,
+                    Status = "Error",
+                    Message = new { Auth = "您沒有營養師權限" }
+                });
+            }
+
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Keys
+                    .Select(key =>
+                    {
+                        var propertyName = key.Split('.').Last(); // 取的屬性名稱
+                        var errorMessage = ModelState[key].Errors.First().ErrorMessage; // 取得錯誤訊息
+                        return new { PropertyName = propertyName, ErrorMessage = errorMessage };
+                    })
+                    .ToDictionary(e => e.PropertyName, e => e.ErrorMessage);
+
+                return Content(HttpStatusCode.BadRequest, new
+                {
+                    StatusCode = 400,
+                    Status = "Error",
+                    Message = errors
+                });
+            }
+
+
+            var nu = db.Nutritionists.FirstOrDefault(n => n.UserId == id);
+            var newPlan = new Plan
+            {
+                NutritionistId = nu.Id,
+                Rank = planView.Rank,
+                CourseName = planView.CourseName,
+                CourseWeek = planView.CourseWeek,
+                CoursePrice = planView.CoursePrice,
+                Detail = planView.Detail,
+                Tag = planView.Tag
+            };
+            db.Plans.Add(newPlan);
+            db.SaveChanges();
+            var result = new
+            {
+                StatusCode = 200,
+                Status = "Success",
+                Message = "營養師課程方案成功",
+                Date = new { newPlan }
+            };
+            return Ok(result);
+        }
+        #endregion "新增API"
+    }
+}
---
+++ NuCares/Models/PlanView.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NuCares.Models
+{
+    public class PlanView
+    {
+        /// <summary>
+        /// 課程排序
+        /// </summary>
+        [Range(1, int.MaxValue, ErrorMessage = "{0}必須為正整数")]
+        [Display(Name = "排序順序")]
+        public int? Rank { get; set; }
+
+        /// <summary>
+        /// 課程名稱
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [StringLength(18, ErrorMessage = "{0}名稱最多 18 個字")]
+        [Display(Name = "課程名稱")]
+        public string CourseName { get; set; }
+
+        /// <summary>
+        /// 課程週數
+        /// </summary>
+        [Range(1, int.MaxValue, ErrorMessage = "{0}必須為正整数")]
+        [Display(Name = "週數")]
+        public int CourseWeek { get; set; }
+
+        /// <summary>
+        /// 價格
+        /// </summary>
+        [Range(1, int.MaxValue, ErrorMessage = "{0}必須為正整数")]
+        [Display(Name = "價格")]
+        public int CoursePrice { get; set; }
+
+        /// <summary>
+        /// 課程說明
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [StringLength(100, ErrorMessage = "{0}最多 100 個字")]
+        [Display(Name = "課程說明")]
+        public string Detail { get; set; }
+
+        /// <summary>
+        /// 標籤
+        /// </summary>
+        [MaxLength(500)]
+        [Display(Name = " 標籤")]
+        public string Tag { get; set; }
+    }
+}
