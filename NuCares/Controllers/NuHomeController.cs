0ffbe3d00a3231db0ef68ff6f6fb975d0180e710
ricks774 <ricks774@gmail.com>
2023-11-03 14:08:03 +0800

    更新登入後收藏營養師判斷

--- NuCares/Controllers/NuHomeController.cs
+++ NuCares/Controllers/NuHomeController.cs
@@ -110,19 +110,19 @@ namespace NuCares.Controllers
             else
             {
                 // 登入時
-                var nuData = db.FavoriteLists
-                    .Where(f => f.UserId == userid)
-                    .OrderBy(f => f.Id) // 主要排序條件
+                var nuData = db.Nutritionists
+                    .Where(n => n.IsPublic)
+                    .OrderBy(n => n.Id) // 主要排序條件
                     .Skip(((int)page - 1) * pageSize) // 跳過前面的記錄
                     .Take(pageSize) // 每頁顯示的記錄數
                     .AsEnumerable() // 使查詢先執行,再在記憶體中處理
-                    .Select(f => new
+                    .Select(n => new
                     {
-                        f.Nutritionist.Title,
-                        PortraitImage = ImageUrl.GetImageUrl(f.Nutritionist.PortraitImage),
-                        Expertise = f.Nutritionist.Expertise.Split(',').ToArray(),
-                        Favorite = f.NutritionistId != 0 ? true : false,
-                        Course = f.Nutritionist.Plans.Select(p => new
+                        n.Title,
+                        PortraitImage = ImageUrl.GetImageUrl(n.PortraitImage),
+                        Expertise = n.Expertise.Split(',').ToArray(),
+                        Favorite = n.FavoriteLists.Where(f => f.UserId == userid).Any(),
+                        Course = n.Plans.Select(p => new
                         {
                             p.Rank,
                             p.CourseName,
