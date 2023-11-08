357e9626f10da6a6960724a76e7d6a9bfda73e55
ricks774 <ricks774@gmail.com>
2023-11-08 10:42:56 +0800

    調整愛心判斷

--- NuCares/Controllers/NuHomeController.cs
+++ NuCares/Controllers/NuHomeController.cs
@@ -154,8 +154,6 @@ namespace NuCares.Controllers
             var nuData = (
                 from n in db.Nutritionists
                 join u in db.Users on n.UserId equals u.Id
-                //join o in db.Orders on u.Id equals o.UserId
-                //join cs in db.Courses on o.Id equals cs.OrderId
                 where n.Id == nutritionistid
                 select new { Nutritionist = n, User = u });   // select出2張表的所有欄位
 
@@ -168,77 +166,66 @@ namespace NuCares.Controllers
                 where p.NutritionistId == nutritionistid
                 select new { User = u, Comment = cm });   // select出2張表的所有欄位
 
+            double rateAvg = commentsData.Select(r => (double)r.Comment.Rate).Average();
+
             // 未登入時
-            if (userid == 0)
-            {
-                //// 取得營養師已被購買的 課程id
-                //var coursesData = db.Plans.Where(p => p.NutritionistId == nutritionistid)
-                //.SelectMany(p => p.Orders)
-                //.SelectMany(o => o.Courses)
-                //.Select(c => c.Id);
-
-                //// 透過 課程id 取得該營養師課程的評價
-                //var commentsData = db.Comments.Where(c => coursesData.Contains(c.CourseId)).AsEnumerable();
-
-                //// 取得 Orders id
-                //var orderIds = db.Courses.Where(c => coursesData.Contains(c.Id)).Select(c => c.OrderId);
-                //// 取得 Users id
-                //var userIds = db.Orders.Where(o => orderIds.Contains(o.Id)).Select(o => o.UserId);
-                //// 取得 Users UserName
-                //var userNameData = db.Users.Where(u => userIds.Contains(u.Id)).Select(u => u.UserName);
-
-                var newNuData = nuData
-                    .AsEnumerable()
-                    .Select(nd => new
+            //if (userid == 0)
+            //{
+            var newNuData = nuData
+                .AsEnumerable()
+                .Select(nd => new
+                {
+                    nd.User.FavoriteLists,
+                    nd.Nutritionist.Title,
+                    PortraitImage = ImageUrl.GetImageUrl(nd.Nutritionist.PortraitImage),
+                    Expertise = nd.Nutritionist.Expertise.Split(',').ToArray(),
+                    Favorite = nd.User.FavoriteLists.Where(f => f.UserId == userid).FirstOrDefault() == null ? false : true,
+                    Favorite2 = nd.User.FavoriteLists.Where(f => f.UserId == userid).FirstOrDefault(),
+                    Gender = nd.User.Gender == 0 ? "男" : "女",
+                    nd.Nutritionist.City,
+                    nd.Nutritionist.Education,
+                    nd.Nutritionist.Experience,
+                    nd.Nutritionist.AboutMe,
+                    nd.Nutritionist.CourseIntro,
+                    Course = nd.Nutritionist.Plans.Where(p => !p.IsDelete).Select(p => new
                     {
-                        nd.Nutritionist.Title,
-                        PortraitImage = ImageUrl.GetImageUrl(nd.Nutritionist.PortraitImage),
-                        Expertise = nd.Nutritionist.Expertise.Split(',').ToArray(),
-                        Favorite = nd.User.FavoriteLists.Where(f => f.UserId == userid).Any(),
-                        Gender = nd.User.Gender == 0 ? "男" : "女",
-                        nd.Nutritionist.City,
-                        nd.Nutritionist.Education,
-                        nd.Nutritionist.Experience,
-                        nd.Nutritionist.AboutMe,
-                        nd.Nutritionist.CourseIntro,
-                        Course = nd.Nutritionist.Plans.Where(p => !p.IsDelete).Select(p => new
-                        {
-                            p.Rank,
-                            p.CourseName,
-                            p.CourseWeek,
-                            p.CoursePrice,
-                            p.Tag
-                        }).OrderBy(p => p.Rank),
-                        Comment = commentsData.AsEnumerable().Select(c => new
-                        {
-                            c.User.UserName,
-                            c.Comment.Content,
-                            c.Comment.Rate,
-                            CreateDate = c.Comment.CreateDate.ToString("yyyy/MM/dd")
-                        })
-                    });
+                        p.Rank,
+                        p.CourseName,
+                        p.CourseWeek,
+                        p.CoursePrice,
+                        p.Tag
+                    }).OrderBy(p => p.Rank),
+                    Comment = commentsData.AsEnumerable().Select(c => new
+                    {
+                        c.User.UserName,
+                        c.Comment.Content,
+                        c.Comment.Rate,
+                        CreateDate = c.Comment.CreateDate.ToString("yyyy/MM/dd")
+                    }),
+                    RateAVG = rateAvg
+                });
 
-                var result = new
-                {
-                    StatusCode = 200,
-                    Status = "Success",
-                    Message = "取得所有營養師",
-                    Data = newNuData
-                };
-                return Ok(result);
-            }
-            else
+            var result = new
             {
-                // 登入時
-                var result = new
-                {
-                    StatusCode = 200,
-                    Status = "Success",
-                    Message = "取得所有營養師",
-                    Data = ""
-                };
-                return Ok("BAD");
-            }
+                StatusCode = 200,
+                Status = "Success",
+                Message = "取得所有營養師",
+                Data = newNuData
+            };
+            return Ok(result);
+            //}
+            //else
+            //{
+            //    // 登入時
+            //    var result = new
+            //    {
+            //        StatusCode = 200,
+            //        Status = "Success",
+            //        Message = "取得所有營養師",
+            //        Data = ""
+            //    };
+            //    return Ok("BAD");
+            //}
         }
 
         #endregion "首頁 - 取得單一營養師"
