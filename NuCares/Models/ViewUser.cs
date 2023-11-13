6eae8fca464df243d27ef03e49f50ef8ab2f9d63
ricks774 <ricks774@gmail.com>
2023-11-13 15:27:25 +0800

    新增_修改使用者資訊API

--- NuCares/Models/ViewUser.cs
+++ NuCares/Models/ViewUser.cs
@@ -117,4 +117,47 @@ namespace NuCares.Models
         [Display(Name = "密碼")]
         public string Password { get; set; }
     }
+
+    public class ViewUserInfoEdit
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [MaxLength(50)]
+        [Display(Name = "姓名")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 生日 yyyy/MM/dd
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "生日")]
+        public DateTime Birthday { get; set; }
+
+        /// <summary>
+        /// 性別：0 男, 1 女
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [Display(Name = "生理性別")]
+        public string Gender { set; get; }
+
+        /// <summary>
+        /// 手機號碼
+        /// </summary>
+        [Required(ErrorMessage = "{0}必填")]
+        [MaxLength(50)]
+        [RegularExpression(@"^09\d{8}$", ErrorMessage = "{0}格式錯誤")]
+        [Display(Name = "手機號碼")]
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// 照片
+        /// </summary>
+        [MaxLength(500)]
+        [Display(Name = "照片")]
+        public string ImgUrl { get; set; }
+    }
 }
