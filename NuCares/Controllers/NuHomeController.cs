e86227f976ed4650572348ae51fb5d597e78e2e2
ricks774 <ricks774@gmail.com>
2023-11-13 09:55:44 +0800

    調整依照Token判斷使用者

--- NuCares/Controllers/NuHomeController.cs
+++ NuCares/Controllers/NuHomeController.cs
@@ -221,12 +221,40 @@ namespace NuCares.Controllers
         /// 取得單一營養師
         /// </summary>
         /// <param name="nutritionistId"></param>
-        /// <param name="userid"></param>
         /// <returns></returns>
         [HttpGet]
         [Route("nutritionist/{nutritionistId}")]
-        public IHttpActionResult GetSingleNu(int nutritionistId, int userid = 0)
+        public IHttpActionResult GetSingleNu(int nutritionistId)
         {
+            int userId = 0;
+
+            #region "JwtToken驗證"
+
+            if (Request.Headers.Authorization != null && !string.IsNullOrEmpty(Request.Headers.Authorization.Parameter))
+            {
+                // 取出請求內容，解密 JwtToken 取出資料
+                var userToken = JwtAuthFilter.GetToken(Request.Headers.Authorization.Parameter);
+                userId = (int)userToken["Id"];
+
+                bool checkUser = db.Users.Any(n => n.Id == userId);
+                if (!checkUser)
+                {
+                    return Content(HttpStatusCode.Unauthorized, new
+                    {
+                        StatusCode = 401,
+                        Status = "Error",
+                        Message = "請重新登入"
+                    });
+                }
+            }
+            else
+            {
+                // 如果沒有 JWT Token，進行相應的處理
+                userId = 0;
+            }
+
+            #endregion "JwtToken驗證"
+
             try
             {
                 // 取得營養師資料
@@ -237,7 +265,7 @@ namespace NuCares.Controllers
                 select new { Nutritionist = n, User = u });   // select出2張表的所有欄位
 
                 // 取得是否為收藏的營養師
-                bool isFavorite = db.FavoriteLists.Where(f => f.UserId == userid && f.NutritionistId == nutritionistId).Any();
+                bool isFavorite = db.FavoriteLists.Where(f => f.UserId == userId && f.NutritionistId == nutritionistId).Any();
 
                 // 取得評價資料
                 var commentsData = (
